b41da18dc851ada576757ebba62fb5c3a10a567e
Aynoble <victordiya90@gmail.com>
2023-01-23 00:49:51 +0100

    completed navBar

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,7 +1,42 @@
 import "./navBar.scss";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
 function NavBar() {
-  return <div className="navBar">NavBar</div>;
+  return (
+    <div className="navBar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>aysocial</span>
+        </Link>
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img
+            src="https://images.unsplash.com/photo-1581803118522-7b72a50f7e9f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+            alt=""
+          />
+          <span>Ayomide</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
