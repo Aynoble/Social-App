a8e14429c0676582b47921c7a43704c7351311fe
Aynoble <victordiya90@gmail.com>
2023-01-23 02:29:59 +0100

    completed leftBar

--- client/src/components/leftBar/LeftBar.jsx
+++ client/src/components/leftBar/LeftBar.jsx
@@ -1,7 +1,94 @@
 import "./leftBar.scss";
+import Friend from "../../img/1.png";
+import Groups from "../../img/2.png";
+import Market from "../../img/3.png";
+import Watch from "../../img/4.png";
+import Memories from "../../img/5.png";
+import Events from "../../img/5.png";
+import Gaming from "../../img/7.png";
+import Gallery from "../../img/8.png";
+import Videos from "../../img/9.png";
+import Messages from "../../img/10.png";
+import Tutorials from "../../img/11.png";
+import Courses from "../../img/12.png";
+import Fund from "../../img/13.png";
 
 function LeftBar() {
-  return <div className="leftBar">LeftBar</div>;
+  return (
+    <div className="leftBar">
+      <div className="container">
+        <div className="menu">
+          <div className="user">
+            <img
+              src="https://images.unsplash.com/photo-1581803118522-7b72a50f7e9f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt=""
+            />
+            <span>Ayomide</span>
+          </div>
+          <div className="item">
+            <img src={Friend} alt="" />
+            <span>Friends</span>
+          </div>
+          <div className="item">
+            <img src={Groups} alt="" />
+            <span>Groups</span>
+          </div>
+          <div className="item">
+            <img src={Market} alt="" />
+            <span>Market</span>
+          </div>
+          <div className="item">
+            <img src={Watch} alt="" />
+            <span>Watch</span>
+          </div>
+          <div className="item">
+            <img src={Memories} alt="" />
+            <span>Memories</span>
+          </div>
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Your shortcuts</span>
+          <div className="item">
+            <img src={Events} alt="" />
+            <span>Events</span>
+          </div>
+          <div className="item">
+            <img src={Gaming} alt="" />
+            <span>Gaming</span>
+          </div>
+          <div className="item">
+            <img src={Gallery} alt="" />
+            <span>Gallery</span>
+          </div>
+          <div className="item">
+            <img src={Videos} alt="" />
+            <span>Videos</span>
+          </div>
+          <div className="item">
+            <img src={Messages} alt="" />
+            <span>Messages</span>
+          </div>
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Others</span>
+          <div className="item">
+            <img src={Fund} alt="" />
+            <span>Fundraiser</span>
+          </div>
+          <div className="item">
+            <img src={Tutorials} alt="" />
+            <span>Tutorials</span>
+          </div>
+          <div className="item">
+            <img src={Courses} alt="" />
+            <span>Courses</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LeftBar;
