7ad1d932c4453f19615ff56e6c487510d1ccb9b8
Aynoble <victordiya90@gmail.com>
2023-01-23 03:03:23 +0100

    commit

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./home.scss";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 }
 
 export default Home;
