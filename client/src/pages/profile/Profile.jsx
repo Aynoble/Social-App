5077e983d1551e19a4b1235c3576e032cb20f5d5
Aynoble <victordiya90@gmail.com>
2023-01-30 13:42:26 +0100

    completed profile page

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,8 +1,73 @@
-import React from "react";
 import "./profile.scss";
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
+import { DarkModeContext } from "../../context/darkModeContext";
+import Posts from "../../components/posts/Posts";
 
 function Profile() {
-  return <div>Profile</div>;
+  const { currentUser } = useContext(AuthContext);
+  const { darkMode } = useContext(DarkModeContext);
+  return (
+    <div className={`theme-${darkMode ? "dark" : "light"} profile`}>
+      <div className="images">
+        <img
+          src="https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d2VhdGhlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+          className="cover"
+        />
+        <img src={currentUser.profilePic} alt="" className="profilePic" />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="#">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="#">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="#">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="#">
+              <LinkedInIcon fontSize="large" />
+            </a>
+            <a href="#">
+              <PinterestIcon fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span>Ayomide</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>Nigeria</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>Ay.dev</span>
+              </div>
+            </div>
+            <button>Follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
