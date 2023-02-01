8fcca471fec90831a5869e04a06f4c2b81e9c3f8
Aynoble <victordiya90@gmail.com>
2023-02-01 13:14:23 +0100

    Perfected responsiveness

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,12 +4,14 @@ import Stories from "../../components/stories/Stories";
 import "./home.scss";
 import "../../style.scss";
 import { DarkModeContext } from "../../context/darkModeContext";
+import CreatePost from "../../components/createPost/CreatePost";
 
 function Home() {
   const { darkMode } = useContext(DarkModeContext);
   return (
     <div className={`theme-${darkMode ? "dark" : "light"} home`}>
       <Stories />
+      <CreatePost />
       <Posts />
     </div>
   );
